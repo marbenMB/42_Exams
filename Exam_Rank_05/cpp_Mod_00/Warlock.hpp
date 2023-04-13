f1cd4860c3709d38ac362cc8445139c21236babd
marbenMB <mbenbajj@student.1337.ma>
2023-04-13 01:47:32 +0000

    Cpp_Mod_00 : Done

--- Exam_Rank_05/cpp_Mod_00/Warlock.hpp
+++ Exam_Rank_05/cpp_Mod_00/Warlock.hpp
@@ -6,19 +6,16 @@
 
 class	Warlock
 {
-	public :
-		Warlock (std::string name, std::string titel);
-		~Warlock ();
-		std::string	const&	getName	(void) const;
-		std::string	const&	getTitle	(void) const;
-		void				setTitle	(std::string const &tl);
-		void				introduce	(void) const;
 	private :
 		std::string	_name;
 		std::string	_title;
-		Warlock ();
-		Warlock (Warlock const& obj);
-		Warlock	&operator=	(Warlock const&	obj);
+	public :
+		Warlock (std::string name, std::string title);
+		~Warlock ();
+		std::string	const&	getName		(void) const;
+		std::string	const&	getTitle	(void) const;
+		void				setTitle	(std::string const &tl);
+		void				introduce	(void) const;
 };
 
 #endif
